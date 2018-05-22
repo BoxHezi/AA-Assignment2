1ccd4881da7cf4b621be869fd283196da31d2b0b
BoxLu <547181293@qq.com>
2018-05-22 20:48:46 +1000

    TO-DO: read chosen person for each player

--- A2/javaSrc-Assignment2/javaSrc/BinaryGuessPlayer.java
+++ A2/javaSrc-Assignment2/javaSrc/BinaryGuessPlayer.java
@@ -1,29 +1,56 @@
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 /**
  * Binary-search based guessing player.
  * This player is for task C.
- *
+ * <p>
  * You may implement/extend other interfaces or classes, but ensure ultimately
  * that this class implements the Player interface (directly or indirectly).
  */
-public class BinaryGuessPlayer implements Player
-{
+public class BinaryGuessPlayer implements Player {
+
+    Map<String, String> featureMap = new HashMap<>();
+
+    Map<String, String> chosen = new HashMap<>();
 
     /**
      * Loads the game configuration from gameFilename, and also store the chosen
      * person.
      *
      * @param gameFilename Filename of game configuration.
-     * @param chosenName Name of the chosen person for this player.
+     * @param chosenName   Name of the chosen person for this player.
      * @throws IOException If there are IO issues with loading of gameFilename.
-     *    Note you can handle IOException within the constructor and remove
-     *    the "throws IOException" method specification, but make sure your
-     *    implementation exits gracefully if an IOException is thrown.
+     *                     Note you can handle IOException within the constructor and remove
+     *                     the "throws IOException" method specification, but make sure your
+     *                     implementation exits gracefully if an IOException is thrown.
      */
-    public BinaryGuessPlayer(String gameFilename, String chosenName)
-        throws IOException
-    {
+    public BinaryGuessPlayer(String gameFilename, String chosenName) throws IOException {
+        Scanner gameFileReader = new Scanner(new File(gameFilename));
+//        Scanner chosenFileReader = new Scanner(new File(chosenName));
+
+        /*while (chosenFileReader.hasNextLine()) {
+            String chosenLine = chosenFileReader.nextLine();
+            String[] chosenStr = chosenLine.split("\\s");
+
+            for (String s : chosenStr) {
+                chosen.put(s, null);
+            }
+            chosenFileReader.close();
+        }*/
+
+        while (gameFileReader.hasNextLine()) {
+            String configLine = gameFileReader.nextLine();
+            String[] attributeFeature = configLine.split("\\s");
+            StringBuilder attributeValue = new StringBuilder();
+            for (int i = 1; i < attributeFeature.length; i++) {
+                attributeValue.append(attributeFeature[i]).append(" ");
+            }
+            featureMap.put(attributeFeature[0], String.valueOf(attributeValue));
+        }
 
     } // end of BinaryGuessPlayer()
 
@@ -35,14 +62,14 @@ public class BinaryGuessPlayer implements Player
     } // end of guess()
 
 
-	public boolean answer(Guess currGuess) {
+    public boolean answer(Guess currGuess) {
 
         // placeholder, replace
         return false;
     } // end of answer()
 
 
-	public boolean receiveAnswer(Guess currGuess, boolean answer) {
+    public boolean receiveAnswer(Guess currGuess, boolean answer) {
 
         // placeholder, replace
         return true;
