3ac011c63aa00ab7920580612dbc9724ce915080
Boxuan Lu <547181293@qq.com>
2018-05-24 16:08:42 +1000

    finish first part of binary guess, need to check bugs

--- A2/javaSrc-Assignment2/javaSrc/BinaryGuessPlayer.java
+++ A2/javaSrc-Assignment2/javaSrc/BinaryGuessPlayer.java
@@ -19,7 +19,9 @@ public class BinaryGuessPlayer implements Player {
     private Map<String, Map<String, Integer>> featureCountMap = new HashMap<>();
     //store each person's feature value
     private Map<String, Map<String, String>> candidateMap = new HashMap<>();
-    int candidateCount = 0;
+    //store all possible answer due to previous answer
+    private Map<String, Map<String, String>> filterMap = new HashMap<>();
+    int candidateCount;
 
     /**
      * Loads the game configuration from gameFilename, and also store the chosen
@@ -72,17 +74,38 @@ public class BinaryGuessPlayer implements Player {
         candidateCount = candidateMap.size();
         chosenFeature = candidateMap.get(chosenName);
 
+        filterMap = new HashMap<>(candidateMap);
+
         calculateFeatureCount();
     } // end of BinaryGuessPlayer()
 
     public Guess guess() {
-
+        //when only one candidate left, guess the person
+        if (filterMap.size() == 1) {
+            String person = "";
+            for (Map.Entry<String, Map<String, String>> entry : filterMap.entrySet()) {
+                person = entry.getKey();
+            }
+            return new Guess(Guess.GuessType.Person, "", person);
+        }
         // placeholder, replace
-        return new Guess(Guess.GuessType.Person, "", "Placeholder");
+
+        int halfNumber = filterMap.size() / 2;
+        List<String> toGuess = getGuessAttributeAndValue(halfNumber);
+
+        return new Guess(Guess.GuessType.Attribute, toGuess.get(0), toGuess.get(1));
     } // end of guess()
 
 
     public boolean answer(Guess currGuess) {
+        if (currGuess.getType() == Guess.GuessType.Person && chosenName.equals(currGuess.getValue())) {
+            return true;
+        }
+
+        if (null != currGuess.getAttribute()) {
+            Map<String, String> tempCandidateMap = candidateMap.get(chosenName);
+            return tempCandidateMap.get(currGuess.getAttribute()).equals(currGuess.getValue());
+        }
 
         // placeholder, replace
         return false;
@@ -90,20 +113,19 @@ public class BinaryGuessPlayer implements Player {
 
 
     public boolean receiveAnswer(Guess currGuess, boolean answer) {
-
-        // placeholder, replace
-        return true;
+        shrinkFilterMap(currGuess, answer);
+        return currGuess.getType() == Guess.GuessType.Person && answer;
     } // end of receiveAnswer()
 
     /**
-     * count feature appearance
+     * calculate feature appearance
      */
     private void calculateFeatureCount() {
         for (String feature : features) {
             featureCountMap.put(feature, new HashMap<>());
         }
-        for (String candidateKey : candidateMap.keySet()) {
-            Map<String, String> tempCandidateMap = candidateMap.get(candidateKey);
+        for (String candidateKey : filterMap.keySet()) {
+            Map<String, String> tempCandidateMap = filterMap.get(candidateKey);
             for (String attribute : tempCandidateMap.keySet()) {
                 String attributeValue = tempCandidateMap.get(attribute);
                 int attributeCount = 0;
@@ -115,8 +137,8 @@ public class BinaryGuessPlayer implements Player {
                 featureCountMap.put(attribute, tempCountMap);
             }
         }
-        for (String s : featureCountMap.keySet() ) {
-            System.out.println(s + " " + featureCountMap.get(s));
+        for (String s : featureCountMap.keySet()) {
+            System.out.println(s + "=" + featureCountMap.get(s));
         }
     }
 
@@ -128,6 +150,61 @@ public class BinaryGuessPlayer implements Player {
     private void storeAttributeType(String features) {
         String[] featureArray = features.split("\\s");
         this.features.addAll(Arrays.asList(featureArray));
+    }
+
+    private void shrinkFilterMap(Guess currGuess, boolean answer) {
+        List<String> candidateList = new ArrayList<>();
+        for (Object obj : filterMap.entrySet()) {
+            Map.Entry mapEntry = (Map.Entry) obj;
+            candidateList.add((String) mapEntry.getKey());
+        }
+        for (String currKey : candidateList) {
+            Map<String, String> tempMap = filterMap.get(currKey);
+            for (Object obj : tempMap.entrySet()) {
+                Map.Entry entry = (Map.Entry) obj;
+                if (answer) {
+                    if (entry.getKey().equals(currGuess.getAttribute())) {
+                        if (!entry.getValue().equals(currGuess.getValue())) {
+                            filterMap.remove(currKey);
+                        }
+                    }
+                } else {
+                    if (entry.getKey().equals(currGuess.getAttribute())) {
+                        if (entry.getValue().equals(currGuess.getValue())) {
+                            filterMap.remove(currKey);
+                        }
+                    }
+                }
+            }
+        }
+        calculateFeatureCount();
+    }
+
+    /**
+     * get best guess attribute and value
+     * @param halfCount half filter indicator
+     * @return list contains attribute and value to guess
+     */
+    private List<String> getGuessAttributeAndValue(int halfCount) {
+        List<String> attributeValue = new ArrayList<>();
+        List<String> mapKey = new ArrayList<>();
+        for (Object obj : featureCountMap.entrySet()) {
+            Map.Entry entry = (Map.Entry) obj;
+            mapKey.add((String) entry.getKey());
+        }
+        for (String currKey : mapKey) {
+            Map<String, Integer> tempMap = featureCountMap.get(currKey);
+            for (Object obj : tempMap.entrySet()) {
+                Map.Entry entry = (Map.Entry) obj;
+                int count = (int) entry.getValue();
+                if (count == halfCount) {
+                    attributeValue.add(currKey);
+                    attributeValue.add((String) entry.getKey());
+                    return attributeValue;
+                }
+            }
+        }
+        return new ArrayList<>();
     }
 
 } // end of class BinaryGuessPlayer
